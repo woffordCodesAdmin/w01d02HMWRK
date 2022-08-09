e9b007b78381a6084d556b4fca8c20cc5c99168e
La_Professor_De_BellaCiao <wofford.codes@gmail.com>
2022-08-09 14:25:22 -0500

    g:

---
+++ syntax.js
@@ -0,0 +1,57 @@
+// This is a Comment
+
+// let myName = "Jordan"
+// let myInstructor = "Josh"
+
+// let theColorOfAnApple = "red"
+//  console.log("I have an apple and its color is ", theColorOfAnApple)
+
+// console.log(myName)
+// console.log(myInstructor)'
+
+
+
+let name = "Jabril"
+let hisAge = 21
+
+let jabrilsSister = "Karen"
+let isAstudent = true
+
+let theNumberTwo = 4 
+// Declaring Variable
+// console.log(theNumberTwo)
+
+theNumberTwo = 2
+// Reassigning the value of the old variable
+
+let userLoggedIn = false 
+
+
+let aaronsQuestion = 10 + theNumberTwo
+
+userLoggedIn = true
+// console.log(aaronsQuestion)
+
+
+
+
+
+
+
+
+
+
+
+// Assignment
+let perscholas = "Tech Company"
+
+let five = 10 / 2
+// Arithmetic
+
+let jordanAge = 7.0
+console.log(5 === 5)  
+console.log(5 !== jordanAge)  
+// Loose => value
+
+ 
+// Strict => value & dataType
